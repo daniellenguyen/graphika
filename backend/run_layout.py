0666b362e4f8ab1a83b7bf7df2085d1b65ff2b4e
Dan <cohen.dan@husky.neu.edu>
2016-12-04 22:32:42 -0500

    normalized node sizes and improved algorithm to reduce gaps in layout

--- backend/run_layout.py
+++ backend/run_layout.py
@@ -8,124 +8,161 @@ input: (graphID : Int), (algorithm : String)
 output: Dict (NodeName -> (X, Y))
 
 algorithm options:
-	"FM^3 (OGDF)"
-	"Fast Multipole Embedder (OGDF)"
-	"Fast Multipole Multilevel Embedder (OGDF)"
-	"Pivot MDS (OGDF)"
-	"GRIP" (kind of slow for larger data sets)
-	- (weird result) "Frutcherman Reingold (OGDF)"
-	- (weird result) "GEM Frick (OGDF)"
-	- (slow) "GEM (Frick)"
-	- (slow) "Bertault (OGDF)"
-	- (slow) "Kamada Kawai (OGDF)"
-	- (slow) "LinLog"
-	- (slow) "Upward Planarization (OGDF)"
-	- (slow) "Visibility (OGDF)"
-	- (InsufficientMemoryException) "Stress Majorization (OGDF)"
-	- (Python stops working?) "Sugiyama (OGDF)"
+    "FM^3 (OGDF)"
+    "Fast Multipole Embedder (OGDF)"
+    "Fast Multipole Multilevel Embedder (OGDF)"
+    "Pivot MDS (OGDF)"
+    "GRIP" (kind of slow for larger data sets)
+    - (weird result) "Frutcherman Reingold (OGDF)"
+    - (weird result) "GEM Frick (OGDF)"
+    - (slow) "GEM (Frick)"
+    - (slow) "Bertault (OGDF)"
+    - (slow) "Kamada Kawai (OGDF)"
+    - (slow) "LinLog"
+    - (slow) "Upward Planarization (OGDF)"
+    - (slow) "Visibility (OGDF)"
+    - (InsufficientMemoryException) "Stress Majorization (OGDF)"
+    - (Python stops working?) "Sugiyama (OGDF)"
 '''
-def runLayout(graphID, algorithm, **extraParams):
-	with open('test_data/%s_links.json' % graphID) as f:
-		linksJson = json.loads(f.read())
+def runLayout(graphID, algorithm):
+    with open('test_data/%s_links.json' % graphID) as f:
+        linksJson = json.loads(f.read())
 
-	g = tlp.newGraph()
-	nameToNode = { node : g.addNode() for node in linksJson.keys() }
+    g = tlp.newGraph()
+    nameToNode = {}
+    nodeToName = {}
+    for name in linksJson.keys():
+        node = g.addNode()
+        nameToNode[name] = node
+        nodeToName[node] = name
 
-	for source, targets in linksJson.items():
-		for target in targets:
-			g.addEdge(nameToNode[source], nameToNode[target])
+    for source, targets in linksJson.items():
+        for target in targets:
+            g.addEdge(nameToNode[source], nameToNode[target])
 
-	layout = g.getLayoutProperty('viewLayout')
-	params = tlp.getDefaultPluginParameters(algorithm, g)
-	params.update(extraParams)
-	
-	successful, errorMsg = g.applyLayoutAlgorithm(algorithm, layout, params)
+    layout = g.getLayoutProperty('viewLayout')
+    params = tlp.getDefaultPluginParameters(algorithm, g)
+    successful, errorMsg = g.applyLayoutAlgorithm(algorithm, layout, params)
 
-	if not successful:
-		raise Exception(errorMsg)
+    if not successful:
+        raise Exception(errorMsg)
 
-	nameToCoords = {}
-	for nodeName in linksJson.keys():
-		x, y, z = layout[nameToNode[nodeName]]
-		nameToCoords[nodeName] = x, y
+    nameToNodeInfo = {}
+    for name, node in nameToNode.items():
+        x, y, z = layout[node]
+        nameToNodeInfo[name] = {
+            'x' : x,
+            'y' : y,
+            'name' : name,
+        }
 
-	normalize(nameToCoords)
-	reduceGaps(nameToCoords)
-	normalize(nameToCoords)
-	# roundify(nameToCoords)
+    calcSizes(nameToNodeInfo, nodeToName, g)
+    normalize(nameToNodeInfo)
+    reduceGaps(nameToNodeInfo)
+    normalize(nameToNodeInfo)
+    # roundify(nameToNodeInfo)
+    # normalize(nameToNodeInfo)
 
-	return nameToCoords
+    return nameToNodeInfo
+
+
+def calcSizes(nameToNodeInfo, nodeToName, graph):
+    maxSize = math.sqrt(tlp.maxDegree(graph) + 1)
+
+    for node in graph.getNodes():
+        name = nodeToName[node]
+        nameToNodeInfo[name]['radius'] = math.sqrt(graph.deg(node) + 1) / maxSize
+
 
 # make all coordinates lie within the range [0, 1]
 def normalize(nodeDict):
-	minX = min(x for x, y in nodeDict.values())
-	minY = min(y for x, y in nodeDict.values())
+    minX = min(info['x'] for info in nodeDict.values())
+    minY = min(info['y'] for info in nodeDict.values())
 
-	# make sure coordinates start at 0
-	for node, (x, y) in nodeDict.items():
-		nodeDict[node] = x - minX, y - minY
+    # make sure coordinates start at 0
+    for info in nodeDict.values():
+        info['x'] -= minX
+        info['y'] -= minY
 
-	maxX = max(x for x, y in nodeDict.values())
-	maxY = max(y for x, y in nodeDict.values())
+    maxX = max(info['x'] for info in nodeDict.values())
+    maxY = max(info['y'] for info in nodeDict.values())
 
-	# make sure coordinates go up to 1
-	for node, (x, y) in nodeDict.items():
-		nodeDict[node] = x / maxX, y / maxY
+    # make sure coordinates go up to 1
+    for info in nodeDict.values():
+        info['x'] /= maxX
+        info['y'] /= maxY
 
 
 def reduceGaps(nodeDict):
-	maxGap = 0.02
+    maxGap = 0.05
 
-	byXCoord = sorted(list(nodeDict.items()), key=lambda node: node[1][0])
-	totalReduction = 0
-	for (_, (prevX, _)), (n, (x, y)) in zip(byXCoord, byXCoord[1:]):
-		totalReduction += max(0, x - prevX - maxGap)
-		nodeDict[n] = x - totalReduction, y
+    proportion = 1 # the fraction of the layout's original size that it currently is
+    byXCoord = sorted(nodeDict.items(), key=lambda kv: kv[1]['x'])
 
-	byYCoord = sorted(list(nodeDict.items()), key=lambda node: node[1][1])
-	totalReduction = 0
-	for (_, (_, prevY)), (n, (x, y)) in zip(byYCoord, byYCoord[1:]):
-		totalReduction += max(0, y - prevY - maxGap)
-		nodeDict[n] = x, y - totalReduction
+    for (_, prevInfo), (n, info) in zip(byXCoord, byXCoord[1:]):
+        before = info['x']
+        info['x'] *= proportion
+
+        if info['x'] - prevInfo['x'] > proportion * maxGap:
+            info['x'] = prevInfo['x'] + proportion * maxGap
+
+        if before:
+            after = info['x']
+            proportion = after / before
+
+    proportion = 1
+    byYCoord = sorted(nodeDict.items(), key=lambda kv: kv[1]['y'])
+
+    for (_, prevInfo), (n, info) in zip(byYCoord, byYCoord[1:]):
+        before = info['y']
+        info['y'] *= proportion
+
+        if info['y'] - prevInfo['y'] > proportion * maxGap:
+            info['y'] = prevInfo['y'] + proportion * maxGap
+
+        if before:
+            after = info['y']
+            proportion = after / before
 
 
 def roundify(nodeDict):
-	for node, (x, y) in nodeDict.items():
-		x -= 0.5
-		y -= 0.5
-		r = max(abs(x), abs(y))
-		x = r * math.cos(math.atan2(y, x))
-		y = r * math.sin(math.atan2(y, x))
-		nodeDict[node] = x, y
+    for info in nodeDict.values():
+        x = info['x'] - 0.5
+        y = info['y'] - 0.5
+        r = max(abs(x), abs(y))
+        info['x'] = r * math.cos(math.atan2(y, x))
+        info['y'] = r * math.sin(math.atan2(y, x))
 
 
-def writeLayout(coords, graphID):
-	with open('./test_data/%s_links.json' % graphID) as f:
-		linksJson = json.loads(f.read())
+def writeLayout(nodeDict, graphID):
+    with open('./test_data/%s_nodes_viz.json' % graphID) as f:
+        nodesJson = json.loads(f.read())
 
-	with open('./test_data/%s_nodes_viz.json' % graphID) as f:
-		nodesJson = json.loads(f.read())
+    output = {}
+    for node, info in nodeDict.items():
+        output[node] = {
+            'x' : info['x'],
+            'y' : info['y'],
+            'radius' : info['radius'],
+            'cluster' : nodesJson[node]['cluster'],
+        }
 
-	output = {}
-	for node, (x, y) in coords.items():
-		output[node] = {
-			'x' : x,
-			'y' : y,
-			'cluster' : nodesJson[node]['cluster'],
-			'default_size' : nodesJson[node]['default_size'],
-		}
+    # with open('test_data/%s_clus_viz.json' % graphID, 'r') as f:
+    #   with open('../views/cluster_colors.json', 'w') as to:
+    #       to.write(f.read())
 
-	with open('test_data/%s_clus_viz.json' % graphID, 'r') as f:
-		with open('../views/cluster_colors.json', 'w') as to:
-			to.write(f.read())
+    # with open('../views/nodes.json'.format(graphID), 'w') as f:
+    #   f.write(json.dumps(output))
 
-	with open('../views/nodes.json'.format(graphID), 'w') as f:
-		f.write(json.dumps(output))
+    with open('test_data/%s_nodes_viz.json' % graphID, 'w') as f:
+        f.write(json.dumps(output))
+
 
 if __name__ == '__main__':
-	graphID = int(sys.argv[1])
-	algorithm = sys.argv[2]
-	print("Running layout algorithm...")
-	coords = runLayout(graphID, algorithm)
-	print("Saving coordinates...")
-	writeLayout(coords, graphID)
+    graphID = int(sys.argv[1])
+    algorithm = sys.argv[2]
+    print("Running layout algorithm...")
+    coords = runLayout(graphID, algorithm)
+    print("Saving coordinates...")
+    writeLayout(coords, graphID)
+    os.system('jython generate_image.py %s' % graphID)
--- views/visualizer.js
+++ views/visualizer.js
@@ -2,15 +2,15 @@
 
 // Defines the dimensions of the visualization
 
-var width = $(window).width(),
-    height = $(window).height();
+var imageSize = Math.min($(window).width(), $(window).height());
+var nodeToImageSize = 0.04
 
 // this is an svg container.
 // holds the visualization.
 var svg = d3.select('body')
             .append('svg')
-            .attr('width', width)
-            .attr('height', height);
+            .attr('width', imageSize)
+            .attr('height', imageSize);
 
     svg.append("rect")
        .attr("width", "100%")
@@ -18,24 +18,24 @@ var svg = d3.select('body')
        .attr("fill", "black");
 
 d3.json("nodes.json", function(error1, graph) {
+  if (error1) throw error1;
+
   d3.json("cluster_colors.json", function(error2, cluster_colors) {
-    
-    if (error1) throw error1;
     if (error2) throw error2;
 
-    // prop is the name of the twitter acct. 
-    // prop is dynamic and we only know it at runtime.
+    //prop is the name of the twitter acct. 
+    //prop is dynamic and we only know it at runtime.
     for (var prop in graph) {
       var node = graph[prop];
       var cx = node.x;
-      var cy = node.y;     
-      var r  = node.default_size;
+      var cy = node.y;
+      var r  = node.radius;
       var color = d3.color("#" + cluster_colors[node.cluster].color);
 
       svg.append("circle")
-           .attr("cx", (width / 2) + (cx * 2500))
-           .attr("cy", (height / 2) + (cy * -1) * 2500)
-           .attr("r", 17 * r)
+           .attr("cx", cx * imageSize)
+           .attr("cy", cy * imageSize)
+           .attr("r",  r  * imageSize * nodeToImageSize)
            .attr("fill", color)
            .attr("stroke", "black");
      }
